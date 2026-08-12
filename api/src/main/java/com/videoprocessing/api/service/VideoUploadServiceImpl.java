9232d9984aa022821618bd0226bc97e5bea809ad
swarajborse <borseswaraj2005@gmail.com>
2026-08-12 19:30:43 +0530

    created processing job and used @transactional for postgres operation

--- api/src/main/java/com/videoprocessing/api/service/VideoUploadServiceImpl.java
+++ api/src/main/java/com/videoprocessing/api/service/VideoUploadServiceImpl.java
@@ -1,29 +1,39 @@
 package com.videoprocessing.api.service;
 
 import com.videoprocessing.api.dto.VideoUploadResponse;
+import com.videoprocessing.api.entity.ProcessingJob;
+import com.videoprocessing.api.entity.ProcessingJobStatus;
 import com.videoprocessing.api.entity.Video;
 import com.videoprocessing.api.entity.VideoStatus;
+import com.videoprocessing.api.repository.ProcessingJobRepository;
 import com.videoprocessing.api.repository.VideoRepository;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.UUID;
+
 
 @Service
 public class VideoUploadServiceImpl implements VideoUploadService {
 
     private final ObjectStorageService objectStorageService;
     private final VideoRepository videoRepository;
+    private final ProcessingJobRepository processingJobRepository;
 
     public VideoUploadServiceImpl(
             ObjectStorageService objectStorageService,
-            VideoRepository videoRepository
+            VideoRepository videoRepository,
+            ProcessingJobRepository processingJobRepository
     ) {
         this.objectStorageService = objectStorageService;
         this.videoRepository = videoRepository;
+        this.processingJobRepository = processingJobRepository;
     }
 
     @Override
+    @Transactional
     public VideoUploadResponse uploadVideo(MultipartFile file) {
 
         if (file == null || file.isEmpty()) {
@@ -33,7 +43,6 @@ public class VideoUploadServiceImpl implements VideoUploadService {
         }
 
         UUID videoId = UUID.randomUUID();
-        UUID jobId = UUID.randomUUID();
 
         String objectKey =
                 "videos/"
@@ -56,11 +65,23 @@ public class VideoUploadServiceImpl implements VideoUploadService {
         video.setContentType(file.getContentType());
         video.setStatus(VideoStatus.PROCESSING);
 
-        Video savedVideo = videoRepository.save(video);
+        Video savedVideo =
+                videoRepository.save(video);
+
+        ProcessingJob job = new ProcessingJob();
+
+        job.setVideo(savedVideo);
+        job.setStatus(ProcessingJobStatus.QUEUED);
+        job.setProgress(0);
+        job.setRetryCount(0);
+        job.setMaxRetries(3);
+
+        ProcessingJob savedJob =
+                processingJobRepository.save(job);
 
         return new VideoUploadResponse(
                 savedVideo.getId(),
-                jobId,
+                savedJob.getId(),
                 savedVideo.getStatus()
         );
     }
